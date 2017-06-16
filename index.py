8fd6b6fcc881368e41f9bfb4deb6a3457a3ee603
FedericoPonzi <isaacisback92@gmail.com>
2017-06-16 13:32:57 +0200

    Fix ping.py

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import os
 import subprocess
+from ping import ping
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,15 +9,17 @@ app = Flask(__name__)
 def check(host=""):
     if len(host) == 0:
         return render_template("index.html")
-    return render_template("check.html", pingres=ping(host), host=host)
+    return render_template("check.html", pingres=doPing(host), host=host)
 
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
 
-def ping(host):
+def doPing(host):
     #....temporary way...
     ipaddress = host # guess who
+    ping(ipaddress);
+    '''
     proc = subprocess.Popen(
         ['ping', '-c', '1', ipaddress],
         stdout=subprocess.PIPE)
@@ -24,7 +27,7 @@ def ping(host):
     if proc.returncode == 0:
         return "up."
     return "down."
-
+    '''
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
--- ping.py
+++ ping.py
@@ -1,4 +1,16 @@
+#!/usr/bin/env python3
+
 """
+    Other Repositories of python-ping
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    * https://github.com/l4m3rx/python-ping      supports Python2 and Python3
+    * https://bitbucket.org/delroth/python-ping
+
+
+    About
+    ~~~~~
+
     A pure python ping implementation using raw socket.
 
 
@@ -24,14 +36,30 @@
     Rewrite by Jens Diemer:
       -> http://www.python-forum.de/post-69122.html#69122
 
+    Rewrite by Johannes Meyer:
+      -> http://www.python-forum.de/viewtopic.php?p=183720
+
 
     Revision history
     ~~~~~~~~~~~~~~~~
+    September 24, 2016
+    Rewrite by ChIdea:
+     - Editied for Python3
+     - Renamed verbose_ping() to ping(). VERBOSE is the switch for verbose/quiet mode.
+     - ping() returns average ms of succeed pings or None on failure.
 
-    March 11, 2010
-    changes by Samuel Stauffer:
-    - replaced time.clock with default_timer which is set to
-      time.clock on windows and time.time on other systems.
+    November 1, 2010
+    Rewrite by Johannes Meyer:
+     -  changed entire code layout
+     -  changed some comments and docstrings
+     -  replaced time.clock() with time.time() in order
+        to be able to use this module on linux, too.
+     -  added global __all__, ICMP_CODE and ERROR_DESCR
+     -  merged functions "do_one" and "send_one_ping"
+     -  placed icmp packet creation in its own function
+     -  removed timestamp from the icmp packet
+     -  added function "multi_ping_query"
+     -  added class "PingQuery"
 
     May 30, 2007
     little rewrite by Jens Diemer:
@@ -39,6 +67,10 @@
      -  replace time.time() with time.clock()
      -  delete "return None" (or change to "return" only)
      -  in checksum() rename "str" to "source_string"
+
+    December 4, 2000
+    Changed the struct.pack() calls to pack the checksum and ID as
+    unsigned. My thanks to Jerome Poincheval for the fix.
 
     November 22, 1997
     Initial hack. Doesn't do much, but rather than try to guess
@@ -51,174 +83,299 @@
     Linux x86. Since I don't know just what's wrong, I'll swap the
     bytes always and then do an htons().
 
-    December 4, 2000
-    Changed the struct.pack() calls to pack the checksum and ID as
-    unsigned. My thanks to Jerome Poincheval for the fix.
-
-    Januari 27, 2015
-    Changed receive response to not accept ICMP request messages.
-    It was possible to receive the very request that was sent.
-
-    Last commit info:
-    ~~~~~~~~~~~~~~~~~
-    $LastChangedDate: $
-    $Rev: $
-    $Author: $
 """
 
+import time
+import socket
+import struct
+import select
+import random
+import asyncore
 
-import os, sys, socket, struct, select, time
-
-if sys.platform == "win32":
-    # On Windows, the best timer is time.clock()
-    default_timer = time.clock
-else:
-    # On most other platforms the best timer is time.time()
-    default_timer = time.time
+VERBOSE = True
 
 # From /usr/include/linux/icmp.h; your milage may vary.
 ICMP_ECHO_REQUEST = 8 # Seems to be the same on Solaris.
 
+ICMP_CODE = socket.getprotobyname('icmp')
+ERROR_DESCR = {
+    1: ' - Note that ICMP messages can only be '
+       'sent from processes running as root.',
+    10013: ' - Note that ICMP messages can only be sent by'
+           ' users or processes with administrator rights.'
+    }
+
+__all__ = ['create_packet', 'do_one', 'ping', 'PingQuery',
+           'multi_ping_query']
+
 
 def checksum(source_string):
-    """
-    I'm not too confident that this is right but testing seems
-    to suggest that it gives the same answers as in_cksum in ping.c
-    """
+    # I'm not too confident that this is right but testing seems to
+    # suggest that it gives the same answers as in_cksum in ping.c.
     sum = 0
-    countTo = (len(source_string)/2)*2
+    l = len(source_string)
+    count_to = (l / 2) * 2
     count = 0
-    source_string = str(source_string)
-    while count < countTo:
-        thisVal = ord(source_string[count + 1])*256 + ord(source_string[count])
-        sum = sum + thisVal
+    while count < count_to:
+        this_val = source_string[count + 1]*256+source_string[count]
+        sum = sum + this_val
         sum = sum & 0xffffffff # Necessary?
         count = count + 2
-
-    if countTo<len(source_string):
-        sum = sum + ord(source_string[len(source_string) - 1])
+    if count_to < l:
+        sum = sum + source_string[l - 1]
         sum = sum & 0xffffffff # Necessary?
-
-    sum = (sum >> 16)  +  (sum & 0xffff)
+    sum = (sum >> 16) + (sum & 0xffff)
     sum = sum + (sum >> 16)
     answer = ~sum
     answer = answer & 0xffff
-
     # Swap bytes. Bugger me if I know why.
     answer = answer >> 8 | (answer << 8 & 0xff00)
-    print(answer)
     return answer
 
 
-def receive_one_ping(my_socket, ID, timeout):
-    """
-    receive the ping from the socket.
-    """
-    timeLeft = timeout
-    while True:
-        startedSelect = default_timer()
-        whatReady = select.select([my_socket], [], [], timeLeft)
-        howLongInSelect = (default_timer() - startedSelect)
-        if whatReady[0] == []: # Timeout
-            return
-
-        timeReceived = default_timer()
-        recPacket, addr = my_socket.recvfrom(1024)
-        icmpHeader = recPacket[20:28]
-        type, code, checksum, packetID, sequence = struct.unpack(
-            "bbHHh", icmpHeader
-        )
-        # Filters out the echo request itself.
-        # This can be tested by pinging 127.0.0.1
-        # You'll see your own request
-        if type != 8 and packetID == ID:
-            bytesInDouble = struct.calcsize("d")
-            timeSent = struct.unpack("d", recPacket[28:28 + bytesInDouble])[0]
-            return timeReceived - timeSent
-
-        timeLeft = timeLeft - howLongInSelect
-        if timeLeft <= 0:
-            return
-
-
-def send_one_ping(my_socket, dest_addr, ID):
-    """
-    Send one ping to the given >dest_addr<.
-    """
-    dest_addr  =  socket.gethostbyname(dest_addr)
-
+def create_packet(id):
+    """Create a new echo request packet based on the given "id"."""
     # Header is type (8), code (8), checksum (16), id (16), sequence (16)
-    my_checksum = 0
-
-    # Make a dummy heder with a 0 checksum.
-    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, my_checksum, ID, 1)
-    bytesInDouble = struct.calcsize("d")
-    data = (192 - bytesInDouble) * "Q"
-    data = struct.pack("d", default_timer()) + data.encode("utf-8") #str
+    header = struct.pack('bbHHh', ICMP_ECHO_REQUEST, 0, 0, id, 1)
+    data = 192 * b'Q'
     # Calculate the checksum on the data and the dummy header.
     my_checksum = checksum(header + data)
-
     # Now that we have the right checksum, we put that in. It's just easier
     # to make up a new header than to stuff it into the dummy.
-    header = struct.pack(
-        "bbHHh", ICMP_ECHO_REQUEST, 0, socket.htons(my_checksum), ID, 1
-    )
-    packet = header + data
-    my_socket.sendto(packet, (dest_addr, 1)) # Don't know about the 1
+    header = struct.pack('bbHHh', ICMP_ECHO_REQUEST, 0,
+                         socket.htons(my_checksum), id, 1)
+    return header + data
 
 
-def do_one(dest_addr, timeout):
+def do_one(dest_addr, timeout=1):
     """
-    Returns either the delay (in seconds) or none on timeout.
+    Sends one ping to the given "dest_addr" which can be an ip or hostname.
+    "timeout" can be any integer or float except negatives and zero.
+
+    Returns either the delay (in seconds) or None on timeout and an invalid
+    address, respectively.
+
     """
-    icmp = socket.getprotobyname("icmp")
     try:
-        my_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp)
-    except socket.error as err:
-        if err.errno == 1:
+        my_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, ICMP_CODE)
+    except socket.error as e:
+        if e.errno in ERROR_DESCR:
             # Operation not permitted
-            msg = err.msg + (
-                " - Note that ICMP messages can only be sent from processes"
-                " running as root."
-            )
-            raise socket.error(msg)
+            raise socket.error(''.join((e.args[1], ERROR_DESCR[e.errno])))
         raise # raise the original error
-
-    my_ID = os.getpid() & 0xFFFF
-
-    send_one_ping(my_socket, dest_addr, my_ID)
-    delay = receive_one_ping(my_socket, my_ID, timeout)
-
+    try:
+        host = socket.gethostbyname(dest_addr)
+    except socket.gaierror:
+        return
+    # Maximum for an unsigned short int c object counts to 65535 so
+    # we have to sure that our packet id is not greater than that.
+    packet_id = int((id(timeout) * random.random()) % 65535)
+    packet = create_packet(packet_id)
+    while packet:
+        # The icmp protocol does not use a port, but the function
+        # below expects it, so we just give it a dummy port.
+        sent = my_socket.sendto(packet, (dest_addr, 1))
+        packet = packet[sent:]
+    delay = receive_ping(my_socket, packet_id, time.time(), timeout)
     my_socket.close()
     return delay
 
 
-def verbose_ping(dest_addr, timeout = 2, count = 4):
-    """
-    Send >count< ping to >dest_addr< with the given >timeout< and display
-    the result.
-    """
-    i = "Up"
-    for i in range(count):
-        print ("ping %s..." % (dest_addr))
-        try:
-            delay  =  do_one(dest_addr, timeout)
-        except socket.gaierror as e:
-            print ("Failed. (socket error: '%s')" % e)
-            break
+def receive_ping(my_socket, packet_id, time_sent, timeout):
+    # Receive the ping from the socket.
+    time_left = timeout
+    while True:
+        started_select = time.time()
+        ready = select.select([my_socket], [], [], time_left)
+        how_long_in_select = time.time() - started_select
+        if ready[0] == []: # Timeout
+            return
+        time_received = time.time()
+        rec_packet, addr = my_socket.recvfrom(1024)
+        icmp_header = rec_packet[20:28]
+        type, code, checksum, p_id, sequence = struct.unpack(
+            'bbHHh', icmp_header)
+        if p_id == packet_id:
+            return time_received - time_sent
+        time_left -= time_received - time_sent
+        if time_left <= 0:
+            return
 
-        if delay  ==  None:
-            print ("failed. (timeout within %d sec.)" % (timeout))
-            i = "Down"
+
+def ping(dest_addr, timeout=2, count=4):
+    """
+    Sends one ping to the given "dest_addr" which can be an ip or hostname.
+
+    "timeout" can be any integer or float except negatives and zero.
+    "count" specifies how many pings will be sent.
+
+    Displays the result on the screen.
+
+    """
+    avg = 0
+    suc = 0
+    for i in range(count):
+        if VERBOSE : print('ping {}...'.format(dest_addr))
+        delay = do_one(dest_addr, timeout)
+        if delay == None:
+            if VERBOSE : print('failed. (Timeout within {} seconds.)'.format(timeout))
         else:
-            i = "Up"
-            delay  =  delay * 1000
-            print ("get ping in %0.4fms" % delay)
-    print()
+            delay = round(delay * 1000.0, 4)
+            avg += delay
+            suc += 1
+            if VERBOSE : print('get ping in {} milliseconds.'.format(delay))
+    return (avg/suc) if avg else None
+
+
+class PingQuery(asyncore.dispatcher):
+    def __init__(self, host, p_id, timeout=0.5, ignore_errors=False):
+        """
+       Derived class from "asyncore.dispatcher" for sending and
+       receiving an icmp echo request/reply.
+
+       Usually this class is used in conjunction with the "loop"
+       function of asyncore.
+
+       Once the loop is over, you can retrieve the results with
+       the "get_result" method. Assignment is possible through
+       the "get_host" method.
+
+       "host" represents the address under which the server can be reached.
+       "timeout" is the interval which the host gets granted for its reply.
+       "p_id" must be any unique integer or float except negatives and zeros.
+
+       If "ignore_errors" is True, the default behaviour of asyncore
+       will be overwritten with a function which does just nothing.
+
+       """
+        asyncore.dispatcher.__init__(self)
+        try:
+            self.create_socket(socket.AF_INET, socket.SOCK_RAW, ICMP_CODE)
+        except socket.error as e:
+            if e.errno in ERROR_DESCR:
+                # Operation not permitted
+                raise socket.error(''.join((e.args[1], ERROR_DESCR[e.errno])))
+            raise # raise the original error
+        self.time_received = 0
+        self.time_sent = 0
+        self.timeout = timeout
+        # Maximum for an unsigned short int c object counts to 65535 so
+        # we have to sure that our packet id is not greater than that.
+        self.packet_id = int((id(timeout) / p_id) % 65535)
+        self.host = host
+        self.packet = create_packet(self.packet_id)
+        if ignore_errors:
+            # If it does not care whether an error occured or not.
+            self.handle_error = self.do_not_handle_errors
+            self.handle_expt = self.do_not_handle_errors
+
+    def writable(self):
+        return self.time_sent == 0
+
+    def handle_write(self):
+        self.time_sent = time.time()
+        while self.packet:
+            # The icmp protocol does not use a port, but the function
+            # below expects it, so we just give it a dummy port.
+            sent = self.sendto(self.packet, (self.host, 1))
+            self.packet = self.packet[sent:]
+
+    def readable(self):
+        # As long as we did not sent anything, the channel has to be left open.
+        if (not self.writable()
+            # Once we sent something, we should periodically check if the reply
+            # timed out.
+            and self.timeout < (time.time() - self.time_sent)):
+            self.close()
+            return False
+        # If the channel should not be closed, we do not want to read something
+        # until we did not sent anything.
+        return not self.writable()
+
+    def handle_read(self):
+        read_time = time.time()
+        packet, addr = self.recvfrom(1024)
+        header = packet[20:28]
+        type, code, checksum, p_id, sequence = struct.unpack("bbHHh", header)
+        if p_id == self.packet_id:
+            # This comparison is necessary because winsocks do not only get
+            # the replies for their own sent packets.
+            self.time_received = read_time
+            self.close()
+
+    def get_result(self):
+        """Return the ping delay if possible, otherwise None."""
+        if self.time_received > 0:
+            return self.time_received - self.time_sent
+
+    def get_host(self):
+        """Return the host where to the request has or should been sent."""
+        return self.host
+
+    def do_not_handle_errors(self):
+        # Just a dummy handler to stop traceback printing, if desired.
+        pass
+
+    def create_socket(self, family, type, proto):
+        # Overwritten, because the original does not support the "proto" arg.
+        sock = socket.socket(family, type, proto)
+        sock.setblocking(0)
+        self.set_socket(sock)
+        # Part of the original but is not used. (at least at python 2.7)
+        # Copied for possible compatiblity reasons.
+        self.family_and_type = family, type
+
+    # If the following methods would not be there, we would see some very
+    # "useful" warnings from asyncore, maybe. But we do not want to, or do we?
+    def handle_connect(self):
+        pass
+
+    def handle_accept(self):
+        pass
+
+    def handle_close(self):
+        self.close()
+
+
+def multi_ping_query(hosts, timeout=1, step=512, ignore_errors=False):
+    """
+    Sends multiple icmp echo requests at once.
+
+    "hosts" is a list of ips or hostnames which should be pinged.
+    "timeout" must be given and a integer or float greater than zero.
+    "step" is the amount of sockets which should be watched at once.
+
+    See the docstring of "PingQuery" for the meaning of "ignore_erros".
+
+    """
+    results, host_list, id = {}, [], 0
+    for host in hosts:
+        try:
+            host_list.append(socket.gethostbyname(host))
+        except socket.gaierror:
+            results[host] = None
+    while host_list:
+        sock_list = []
+        for ip in host_list[:step]: # select supports only a max of 512
+            id += 1
+            sock_list.append(PingQuery(ip, id, timeout, ignore_errors))
+            host_list.remove(ip)
+        # Remember to use a timeout here. The risk to get an infinite loop
+        # is high, because noone can guarantee that each host will reply!
+        asyncore.loop(timeout)
+        for sock in sock_list:
+            results[sock.get_host()] = sock.get_result()
+    return results
 
 
 if __name__ == '__main__':
-    verbose_ping("heise.de")
-    verbose_ping("google.com")
-    verbose_ping("a-test-url-taht-is-not-available.com")
-    verbose_ping("192.168.1.1")
+    VERBOSE = True
+    # Testing
+    ping('www.heise.de'); print('')
+    ping('google.com'); print('')
+    ping('an-invalid-test-url.com'); print('')
+    ping('127.0.0.1'); print('')
+    host_list = ['www.heise.de', 'google.com', '127.0.0.1',
+                 'an-invalid-test-url.com']
+    for host, ping in multi_ping_query(host_list).items():
+        print(host, '=', ping)
